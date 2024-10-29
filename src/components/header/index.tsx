cfd2c9509d85015827b8443c8f2d19ad06223640
Santiago Souto <souto.santiago@academico.ifpb.edu.br>
2024-10-28 23:38:51 -0300

    Feat: Header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,25 +1,27 @@
 import Image from 'next/image'
-import logo from '/logo.png'
+import logo from '@/assets/logo.svg'
 import Link from 'next/link'
 import { Button } from '../ui/button'
 
 export default function Header() {
   return (
-    <header>
-      <div className="container flex items-center justify-center">
-        <Image alt='Logo da Eclipit' src={logo} />
-        <nav>
-          <Link href='#inicio'>
+    <header className='w-full'>
+      <div className="container grid grid-cols-3 items-center justify-center mx-auto py-5">
+        <Link href='/'>
+          <Image alt='Logo da Eclipit' src={logo} />
+        </Link>
+        <nav className='mx-auto flex items-center gap-10'>
+          <Link className='hover:-translate-y-1 transition-all' href='#inicio'>
             Inicio
           </Link>
-          <Link href='#servicos'>
+          <Link className='hover:-translate-y-1 transition-all' href='#servicos'>
             Serviços
           </Link>
-          <Link href='#sobre'>
+          <Link className='hover:-translate-y-1 transition-all' href='#sobre'>
             Sobre
           </Link>
         </nav>
-        <Button variant={'ghost'}>
+        <Button variant={'ghost'} className='flex justify-end w-fit ml-auto'>
           Contato
         </Button>
       </div>
