87fdd4887bfc1d85bc2e1840d5903ce436310a46
Santiago Souto <souto.santiago@academico.ifpb.edu.br>
2025-01-22 19:29:57 -0300

    script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,12 +20,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <script type="text/javascript" async src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/756429b9-1b98-4282-a7ae-08bef1a6b99e-loader.js" ></script>
       <body
         className={`${jakartaSans.className} antialiased text-[#F5F5F5] w-full flex flex-col justify-center px-5 overflow-x-hidden`}
       >
         {children}
         <Toaster />
+        <script type="text/javascript" async src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/756429b9-1b98-4282-a7ae-08bef1a6b99e-loader.js" ></script>
       </body>
     </html>
   );
